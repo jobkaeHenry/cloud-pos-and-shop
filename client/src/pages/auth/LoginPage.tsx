ccd58f4d034aa2a411e430d2ad3b7859935f0948
Jungu Lee <1zzangjun@gmail.com>
2024-01-10 07:06:14 +0900

    Minor : Image URL 변경

--- client/src/pages/auth/LoginPage.tsx
+++ client/src/pages/auth/LoginPage.tsx
@@ -6,7 +6,7 @@ const LoginPage = () => {
 
   return (
     <div className="flex flex-row w-full justify-center  bg-gray-100 md:bg-white">
-      <div className="flex-grow-1-1 w-full hidden md:block bg-[url('https://payhere.in/images/main/img_payment_bg.webp')] bg-no-repeat bg-cover bg-center" />
+      <div className="flex-grow-1-1 w-full hidden md:block bg-[url('https://cdn.pixabay.com/photo/2016/10/10/22/37/credit-card-1730085_960_720.jpg')] bg-no-repeat bg-cover bg-center" />
       <main className="bg-white p-8 py-24 rounded shadow-md min-h-screen w-full md:max-w-lg">
         <LoginForm onSubmit={loginHandler} />
       </main>
